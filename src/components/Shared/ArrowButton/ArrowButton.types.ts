b2c6ed9ec900f800539b989a82eab0044978a17c
Valeriia Dubrovskaia <sphen0@yandex.ru>
2023-04-21 16:10:01 +0600

    create arrow-button

---
+++ src/components/Shared/ArrowButton/ArrowButton.types.ts
@@ -0,0 +1,9 @@
+//import { ReactNode } from 'react';
+
+export interface ArrowButtonProps {
+  //title: string;
+  //titleClassName?: string;
+  buttonClassName?: string;
+  onClick?(): void;
+  //children?: ReactNode;
+}
