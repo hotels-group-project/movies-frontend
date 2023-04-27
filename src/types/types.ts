111fa6dd939671b40bfc04a994b069ca072e0d14
Valeriia Dubrovskaia <sphen0@yandex.ru>
2023-04-27 14:31:56 +0500

    modified types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,23 @@
+import { ReactNode } from 'react';
+
 export interface Breakpoint {
   isDesktop: boolean;
   isTablet: boolean;
+}
+
+export enum DropdownMenuTypes {
+  'my-ivi' = 'my-ivi',
+  'new' = 'new',
+  'movies' = 'movies',
+  'serial-movies' = 'serial-movies',
+  'cartoons' = 'cartoons',
+  'tvplus' = 'tvplus',
+  'bell' = 'bell',
+  'user' = 'user',
+}
+
+export interface DropdownMenu {
+  dropdownMenuHoveredItem: keyof typeof DropdownMenuTypes | '';
 }
 
 export type Variant =
@@ -15,3 +32,13 @@ export type Variant =
   | 'sort_circle'
   | 'sort_square'
   | 'text';
+
+export interface ButtonAndLinkProps {
+  variant?: Variant;
+  link: string;
+  className?: string;
+  onClick?(): void;
+  startIcon?: ReactNode;
+  children?: ReactNode;
+  endIcon?: ReactNode;
+}
