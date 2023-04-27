f99ee332124f57b93ade83f0d588ce1d076f5394
Yulia <K9063140517@yandex.ru>
2023-04-27 17:39:40 +0400

    fix: delete onClick LinkComponent

--- src/components/Shared/LinkComponent/LinkComponent.tsx
+++ src/components/Shared/LinkComponent/LinkComponent.tsx
@@ -5,21 +5,9 @@ import { ButtonAndLinkProps } from '../../../types/types';
 
 import styles from './LinkComponent.module.scss';
 
-const LinkComponent: FC<ButtonAndLinkProps> = ({
-  link,
-  variant,
-  elemClassName,
-  onClick,
-  startIcon,
-  children,
-  endIcon,
-}) => {
+const LinkComponent: FC<ButtonAndLinkProps> = ({ link, variant, elemClassName, startIcon, children, endIcon }) => {
   return (
-    <Link
-      href={link}
-      onClick={onClick}
-      className={`${variant && styles[`${variant}`]} ${elemClassName ? elemClassName : ''}`}
-    >
+    <Link href={link} className={`${variant && styles[`${variant}`]} ${elemClassName ? elemClassName : ''}`}>
       {startIcon && <span>{startIcon}</span>}
       {children}
       {endIcon && <span>{endIcon}</span>}
