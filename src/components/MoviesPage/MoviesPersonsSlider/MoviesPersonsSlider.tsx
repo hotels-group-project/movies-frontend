33646f72968760f024edbdcda35313c3e6674c3c
Yulia <K9063140517@yandex.ru>
2023-05-23 19:57:47 +0400

    add change words end, fix styles

--- src/components/MoviesPage/MoviesPersonsSlider/MoviesPersonsSlider.tsx
+++ src/components/MoviesPage/MoviesPersonsSlider/MoviesPersonsSlider.tsx
@@ -4,26 +4,25 @@ import { FC, memo } from 'react';
 import { SwiperSlide } from 'swiper/react';
 
 import { useAppSelector } from '../../../hooks/redux';
+import { moviesEndingChange } from '../../../utils/helpers';
 
 import Slider from '../../Shared/Slider/Slider';
 
 import styles from './MoviesPersonsSlider.module.scss';
-
-export const BASE_URL = 'http://localhost:3000';
 
 const MoviePersonsSlider: FC = () => {
   const persons = useAppSelector(state => state.persons).persons;
   const slides = persons.map(item => {
     return (
       <SwiperSlide key={item.person_id} className={styles.moviesPersonsSliderSlide}>
-        <Link href={`${BASE_URL}/person=${item.firstName}${item.lastName}`}>
+        <Link href={`/actor/${item.firstName}${item.lastName}`}>
           <div className={styles.moviesPersonsSliderWrapper}>
             <Image
               className={styles.moviesPersonsSliderImg}
               src={item.photo}
               alt="person"
-              width={240}
-              height={380}
+              width={170}
+              height={170}
               priority
               loading="eager"
             />
@@ -34,7 +33,9 @@ const MoviePersonsSlider: FC = () => {
           </div>
           <h3 className={styles.moviesPersonsSliderName}>{item.firstName}</h3>
           <h3 className={styles.moviesPersonsSliderName}>{item.lastName}</h3>
-          <p className={styles.moviesPersonsSliderFilmsAmount}>{item.films_count} фильмов</p>
+          <p className={styles.moviesPersonsSliderFilmsAmount}>
+            {item.films_count} {moviesEndingChange(item.films_count)}
+          </p>
         </Link>
       </SwiperSlide>
     );
@@ -46,7 +47,7 @@ const MoviePersonsSlider: FC = () => {
         <Slider
           slidesCount={7}
           slidesPerView="auto"
-          spaceBetween={24}
+          spaceBetween={1}
           sliderClassName={styles.moviesPersonsSlider}
           prevButtonClassName={styles.prevButton}
           nextButtonClassName={styles.nextButton}
