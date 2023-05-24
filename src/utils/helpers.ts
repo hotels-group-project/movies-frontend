086f5a9a00a9806022ae287dc3bf577905f343aa
Yulia <K9063140517@yandex.ru>
2023-05-24 15:55:11 +0400

    fix: translate section title and movies count in persons

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -8,11 +8,7 @@ export const changePlusToUnicode = (str: string) => {
   return str.replace(/\+/g, '%2B');
 };
 
-const wordEndingChange = (number: number, txt: Array<string>) => {
+export const wordEndingChange = (number: number, txt: Array<string>) => {
   const cases = [2, 0, 1, 1, 1, 2];
   return txt[number % 100 > 4 && number % 100 < 20 ? 2 : cases[number % 10 < 5 ? number % 10 : 5]];
-};
-
-export const moviesEndingChange = (number: number) => {
-  return wordEndingChange(number, ['фильм', 'фильма', 'фильмов']);
 };
